1b06696f8b09a910a4e98350a49d2ab79563dfcf
Alan Madrigal <alan_madrigal@umail.ucsb.edu>
2017-05-30 00:50:29 -0700

    implemented versions

--- moreLinkedListFuncs.cpp
+++ moreLinkedListFuncs.cpp
@@ -15,15 +15,24 @@ Node * pointerToMax(LinkedList *list) {
   //  so does not need to do error checking for these conditions.
   assert(list!=NULL);
   assert(list->head != NULL);
-
+  Node *p,*r;
+  p = list->head;
+  r=list->head;
+  int maxVal=p->data;
+  while(p != NULL){
+    if(p->data > maxVal){
+      maxVal = p->data;
+      r=p;
+    }
+    p = p->next;
+  }
+  return r;
+} 
   // TODO: Insert code here to calculate and return
   //   value of pointer to max element (first one if ties.)
+ 
 
-  return NULL; // STUB!  Replace this line with correct code
-}
-
-// list: ptr to a linked list of Node (each with int data, and Node * next)
-// Return a pointer to node with the smallest value.  
+// list: ptr to a linked list of Node (each with int data, and Node * next)     // Return a pointer to node with the smallest value.  
 // You may assume list has at least one element
 // If more than one element has smallest value, 
 //  break tie by returning a pointer to the one the occurs
@@ -37,14 +46,25 @@ Node * pointerToMin(LinkedList *list) {
 
   assert(list!=NULL);
   assert(list->head != NULL);
+  Node *p,*r;
+  p = list->head;
+  r=list->head;
+  int minVal=p->data;
+  while(p != NULL){
+    if(p->data < minVal){
+      minVal = p->data;
+      r=p;
+    }
+    p = p->next;
+  }
+  return r;
+}
+
 
   // TODO: Insert code here to calculate and return
   //   value of pointer to min element 
   //   (first one such value that occurs if there are ties.)
 
-  return NULL; // STUB!  Replace this line with correct code
-
-}
 
 // list: ptr to a linked list of Node (each with int data, and Node * next)
 // Return the largest value in the list.
@@ -58,12 +78,21 @@ int largestValue(LinkedList *list) {
 
   assert(list!=NULL);
   assert(list->head != NULL);
+  Node *p;
+  p = list->head;
+  int maxVal=p->data;
+  while(p != NULL){
+    if(p->data > maxVal){
+      maxVal = p->data;
+    }
+    p = p->next;
+  }
+
 
   // TODO: Insert code here to calculate and return
   //   largest value in list (which may not be unique).
 
-  return -42; // STUB!  Replace this line with correct code
-
+  return maxVal;
 }
 
 // list: ptr to a linked list of Node (each with int data, and Node * next)
@@ -77,11 +106,20 @@ int smallestValue(LinkedList *list) {
 
   assert(list!=NULL);
   assert(list->head != NULL);
+  Node *p;
+  p = list->head;
+  int minVal=p->data;
+  while(p != NULL){
+    if(p->data < minVal){
+      minVal = p->data;
+    }
+    p = p->next;
+  }
 
   // TODO: Insert code here to calculate and return
   //   smallest value in list (which may not be unique).
 
-  return -42; // STUB!  Replace this line with correct code
+  return minVal;
 
 }
 
@@ -96,11 +134,42 @@ int sum(LinkedList * list) {
   //  so does not need to do error checking for these conditions.
 
   assert(list!=NULL);
+  Node *p;
+  p = list->head;
+  int sum=0;
+  if(p==NULL){
+    return sum;
+  }
+  else{
+    while(p != NULL){
+      sum+= p->data;
+      p = p->next;
+    }
+  }
 
   // TODO: Insert code here to calculate and return
   //   sum of all values in list (0 if there are none).
 
-  return -42; // STUB!  Replace this line with correct code
+  return sum; // STUB!  Replace this line with correct code
 
 }
+
+//Precondition: A pointer to a LinkedList object created on the heap that
+////contains all nodes in sorted (ascending) order. The list may be empty.
+////The list may have multiple copies of the same element
+////Postcondition: Insert a node with the given value at the appropriate
+////location in the list, so that the new list also contains nodes in sorted
+////(ascending) order. The list may have multiple copies of the same element
+
+void insertNodeToSortedList(LinkedList *list, int value);
+  Node *p;
+  Node *val= new Node;
+  val->data=value;
+  p = list->head;
+  while(p != NULL){
+    if(p->data >value){
+      p->next=val->data;
+    }
+    p = p->next;
+  }
 
